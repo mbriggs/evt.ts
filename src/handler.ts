a8fc13d5d4decd8a8ed003dff8498a3f5257d3e5
Matt Briggs <matt@mattbriggs.net>
2021-01-27 22:43:41 -0500

    Handlers can support projections

--- src/handler.ts
+++ src/handler.ts
@@ -1,30 +1,37 @@
 import * as mesg from "./messaging";
 import * as mdb from "./message-db";
 
-export type Handler = (msg: mdb.Message) => void;
-export interface HandlerBuilder {
-  handler(): Handler;
+export type Handler<C, R = void> = (msg: mdb.Message, ctx: C) => Promise<R> | R;
+export interface HandlerBuilder<C, R = void> {
+  handler(): Handler<C, R>;
 }
 
-export type MessageHandler<T extends mesg.Message> = (msg: T) => void;
+export type MessageHandler<T extends mesg.Message, C, R = void> = (
+  msg: T,
+  ctx: C
+) => Promise<R> | R;
+
 export interface UserType<T extends mesg.Message = any> {
   fromMessageDB(msg: mdb.Message): T;
   new (...args: any[]): T;
   name: string;
 }
 
-export class Dispatcher implements HandlerBuilder {
+export class Dispatcher<C, R = void> implements HandlerBuilder<C, R> {
   userTypes = new Map<string, UserType>();
-  messageHandlers = new Map<string, MessageHandler<any>>();
+  messageHandlers = new Map<string, MessageHandler<any, C, R>>();
 
-  handle<T extends mesg.Message>(cls: UserType<T>, handler: (msg: T) => void) {
+  handle<T extends mesg.Message>(
+    cls: UserType<T>,
+    handler: (msg: T, ctx: C) => R | Promise<R>
+  ) {
     let type = cls.name;
     this.userTypes.set(type, cls);
     this.messageHandlers.set(type, handler);
   }
 
-  handler(): Handler {
-    return (data: mdb.Message) => {
+  handler(): Handler<C, R> {
+    return (data: mdb.Message, ctx?: any) => {
       if (!this.userTypes.has(data.type)) {
         return;
       }
@@ -34,7 +41,9 @@ export class Dispatcher implements HandlerBuilder {
 
       let msg = cls.fromMessageDB(data);
 
-      handler(msg);
+      let result = handler(msg, ctx);
+
+      return result;
     };
   }
 }
